d6e7f152fe2c5cd31e49ac2ac826f27b8f736cc8
OlgaMarshenina <76002055+OlgaMarshenina@users.noreply.github.com>
2021-01-18 18:29:42 +0400

    Add files via upload

--- Main.java
+++ Main.java
@@ -2,16 +2,84 @@ package com.company;
 
 public class Main {
 
-	// Задать целочисленный массив, состоящий из элементов 0 и 1
-        // Например: [ 1, 1, 0, 0, 1, 0, 1, 1, 0, 0 ]. С помощью цикла и условия заменить 0 на 1, 1 на 0;
+    public static void main(String[] args) {
 
-        public static void invertArray() {
-            int[] arr = { 1, 1, 0, 0, 1, 0, 1, 1, 0, 0};
-            for (int i = 0; i < arr.length; i++) {
-                if(arr[i] == 1){
-                    return arr[i] = 0;
-                }
-            }
-            System.out.println(arr);
+        //Создать переменные всех пройденных типов данных и инициализировать их значения.
+        byte b = 101;
+        short s = 1123;
+        int i = 64536;
+        long l = 2147483648L;
+        double d = 4.1;
+        float f = 3.14f;
+        boolean bool = true;
+        char ch = 'c';
+        String a = "Hello";
+        System.out.println("Значение для типа byte = " + b);
+        System.out.println("Значение для типа short = " + s);
+        System.out.println("Значение для типа int = " + i);
+        System.out.println("Значение для типа long = " + l);
+        System.out.println("Значение для типа double = " + d);
+        System.out.println("Значение для типа float = " + f);
+        System.out.println("Значение для типа boolean = " + b);
+        System.out.println("Значение для типа char = " + ch);
+        System.out.println("Значение для типа String = " + a);
+
+        //Написать метод, которому в качестве параметра передается строка, обозначающая имя.
+        //Метод должен вывести в консоль сообщение «Привет, указанное_имя!».
+
+        String name = "Ольга";
+        System.out.println("Привет, " + name + "!");
+
+    }
+
+        //Написать метод вычисляющий выражение a * (b + (c / d)) и возвращающий результат,
+        //где a, b, c, d – аргументы этого метода, имеющие тип float.
+        static float doTwo ( float a, float b, float c, float d){
+
+            return a * (b + (c / d));
+
         }
-}
+
+        //Написать метод, принимающий на вход два целых числа и проверяющий,
+        //что их сумма лежит в пределах от 10 до 20 (включительно), если да – вернуть true, в противном случае – false.
+        static boolean doThree ( int a, int b){
+            int sum = a + b;
+            if (sum >= 10 && sum <= 20) {
+                return true;
+            } else {
+                return false;
+
+            }
+        }
+        //Написать метод, которому в качестве параметра передается целое число, метод должен напечатать в консоль,
+        //положительное ли число передали или отрицательное.
+        //Замечание: ноль считаем положительным числом.
+
+
+        static void doFour ( int a){
+            if (a >= 0) {
+                System.out.println("Число" + a + "положительное");
+            } else {
+                System.out.println("Число" + a + "отрицательное");
+            }
+        }
+
+        //Написать метод, которому в качестве параметра передается целое число.
+        //Метод должен вернуть true, если число отрицательное, и вернуть false если положительное.
+        static boolean doFive ( int a){
+            if (a < 0) {
+                return true;
+            } else return false;
+        }
+
+
+    }
+
+
+
+
+
+
+
+
+
